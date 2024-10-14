c47a810f3989c4f10a44af56ea982c3ce4c042c1
adamfitriyono <adamuixprojects@gmail.com>
2024-10-14 19:42:07 +0700

    update section Hero

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
 const nav = document.getElementById('nav');
 
-window.addEventListener('scroll', function(){
-    scrollposition = window.scrollY;
+window.addEventListener('scroll', function () {
+  scrollposition = window.scrollY;
 
-    if ( scrollposition >= 60 ) {
-        nav.classList.add('nav-dark');
-    } else if (scrollposition <= 60) {
-        nav.classList.remove('nav-dark')
-    }
-})
+  if (scrollposition >= 60) {
+    nav.classList.add('nav-dark');
+  } else if (scrollposition <= 60) {
+    nav.classList.remove('nav-dark');
+  }
+});
 
 // Tambahkan event listener untuk efek ripple
 document.querySelector('.btn-custom1').addEventListener('click', function (e) {
-    const button = e.currentTarget;
+  const button = e.currentTarget;
 
-    // Mengatur posisi ripple
-    const rect = button.getBoundingClientRect();
-    const ripple = button.querySelector('::after');
-    
-    ripple.style.left = `${e.clientX - rect.left}px`;
-    ripple.style.top = `${e.clientY - rect.top}px`;
+  // Mengatur posisi ripple
+  const rect = button.getBoundingClientRect();
+  const ripple = button.querySelector('::after');
 
-    // Menambahkan kelas untuk animasi ripple
-    button.classList.add('ripple-active');
+  ripple.style.left = `${e.clientX - rect.left}px`;
+  ripple.style.top = `${e.clientY - rect.top}px`;
 
-    // Hapus kelas setelah animasi selesai
-    setTimeout(() => {
-        button.classList.remove('ripple-active');
-    }, 600); // Durasi sesuai dengan CSS transition
+  // Menambahkan kelas untuk animasi ripple
+  button.classList.add('ripple-active');
+
+  // Hapus kelas setelah animasi selesai
+  setTimeout(() => {
+    button.classList.remove('ripple-active');
+  }, 600); // Durasi sesuai dengan CSS transition
 });
-
